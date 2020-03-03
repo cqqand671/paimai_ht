9ace41d557b1d351297c12548a3bdf14a007ef0c
cqqand671 <1907893733@qq.com>
2020-03-03 14:56:09 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/Paimai2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //boot启动
+public class Paimai2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paimai2Application.class, args);
+	}
+
+}
